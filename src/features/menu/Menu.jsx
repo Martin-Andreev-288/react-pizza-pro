2bfc0b87e0b0f3d158d022ee28c057f5b92d5852
Martin Andreev <martin_i_a@abv.bg>
2024-09-26 01:52:17 +0300

    Setup the menu styling
    Refactor the button component so that it could be more versatile.
    Now we have three type of buttons (primary, small and secondary) which are setted by types in the places where they are used.

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,10 +1,11 @@
-import { useLoaderData } from "react-router-dom";
-import { getMenu } from "../../services/apiRestaurant";
-import MenuItem from "./MenuItem";
+import { useLoaderData } from 'react-router-dom';
+import { getMenu } from '../../services/apiRestaurant';
+import MenuItem from './MenuItem';
 function Menu() {
   const menu = useLoaderData();
+
   return (
-    <ul>
+    <ul className="divide-y divide-stone-200 px-2">
       {menu.map((pizza) => (
         <MenuItem pizza={pizza} key={pizza.id} />
       ))}
@@ -16,4 +17,5 @@ export async function loader() {
   const menu = await getMenu();
   return menu;
 }
+
 export default Menu;
